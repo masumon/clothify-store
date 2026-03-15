55ccf3ae44cd592aa13d0430db59d9cdc2eea15f
MUMAIN AHMED <37773524+masumon@users.noreply.github.com>
2026-03-16 02:18:11 +0600

    add home page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{padding:"40px",textAlign:"center"}}>
+      <h2>Welcome to Clothify</h2>
+      <p>Your online fashion store</p>
+    </main>
+  );
+}
