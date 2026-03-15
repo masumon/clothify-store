8fb41c8d7cd2538c3a3d6140b2166f2b2306e733
MUMAIN AHMED <37773524+masumon@users.noreply.github.com>
2026-03-16 02:36:23 +0600

    update layout
    Updated the layout component to include a background class and removed inline styles.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,21 +2,17 @@ import "./globals.css";
 
 export const metadata = {
   title: "Clothify",
-  description: "Find Your Fit",
+  description: "Find Your Fit"
 };
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
+}:{
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body>
-        <header style={{background:"black",color:"white",padding:"20px",textAlign:"center"}}>
-          <h1>Clothify</h1>
-          <p>Find Your Fit</p>
-        </header>
+      <body className="bg-gray-100">
 
         {children}
 
