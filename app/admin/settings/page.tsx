d191f7ded6195f4b4bfffc28fad6916f330fade6
MUMAIN AHMED <37773524+masumon@users.noreply.github.com>
2026-03-16 02:55:04 +0600

    update admin settings page with form

--- app/admin/settings/page.tsx
+++ app/admin/settings/page.tsx
@@ -1,27 +1,15 @@
+import StoreSettingsForm from "@/components/StoreSettingsForm";
+import AdminTopbar from "@/components/AdminTopbar";
 import { getStoreSettings } from "@/lib/data";
 
 export default async function AdminSettingsPage() {
-
   const settings = await getStoreSettings();
 
   return (
     <section>
-      <h1 className="text-3xl font-bold mb-6">Store Settings</h1>
-
-      <div className="bg-white p-6 rounded-xl border">
-
-        <p><b>Store Name:</b> {settings?.store_name}</p>
-        <p><b>Slogan:</b> {settings?.slogan}</p>
-        <p><b>Phone:</b> {settings?.contact_phone}</p>
-        <p><b>bKash:</b> {settings?.bkash_number}</p>
-
-        <img
-          src={settings?.logo_url}
-          className="mt-4 w-32"
-        />
-
-      </div>
-
+      <AdminTopbar />
+      <h1 className="mb-6 text-3xl font-bold text-slate-900">Store Settings</h1>
+      <StoreSettingsForm initialData={settings} />
     </section>
   );
 }
