2665d676c0ec9f6b8401423561a1a63ef3c2178d
Mumain Ahmed <m.a.sumon92@gmail.com>
2026-03-17 03:35:06 +0600

    auto-sync 2026-03-17 03:35:06

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
 const remotePatterns = [];
 
 if (process.env.NEXT_PUBLIC_SUPABASE_URL) {
@@ -19,4 +26,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
